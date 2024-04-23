a690cfac39fe560e8d4e9e84fa520869983e4876
Mike H <socksoup22@gmail.com>
2024-04-23 12:54:23 -0400

    cleanup on modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
-
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
@@ -9,24 +7,23 @@ export const dynamic = "force-dynamic";
 
 async function Images() {
   const images = await getMyImages();
+
   return (
-    <div className="flex flex-wrap justify-center gap-4">
-      {images.map((image) => {
-        return (
-          <div key={image.id} className="flex h-48 w-48 flex-col">
-            <Link href={`/img/${image.id}`}>
-              <Image
-                src={image.url}
-                style={{ objectFit: "contain" }}
-                width={192}
-                height={192}
-                alt={image.name}
-              />
-            </Link>
-            <div>{image.name}</div>
-          </div>
-        );
-      })}
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {images.map((image) => (
+        <div key={image.id} className="flex h-48 w-48 flex-col">
+          <Link href={`/img/${image.id}`}>
+            <Image
+              src={image.url}
+              style={{ objectFit: "contain" }}
+              width={192}
+              height={192}
+              alt={image.name}
+            />
+          </Link>
+          <div>{image.name}</div>
+        </div>
+      ))}
     </div>
   );
 }
@@ -36,7 +33,7 @@ export default async function HomePage() {
     <main className="">
       <SignedOut>
         <div className="h-full w-full text-center text-2xl">
-          <h1>Please sign in</h1>
+          Please sign in above
         </div>
       </SignedOut>
       <SignedIn>
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,7 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
   // eslint-disable-next-line @next/next/no-img-element
-  return <img src={image.url} className="w-96" alt="pic" />;
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} className="w-96 object-contain" alt="pic" />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
