79f311d09927d6df0253981ab0c4cf9f5f4d6c3c
Mike H <socksoup22@gmail.com>
2024-04-25 15:10:18 -0400

    updated sentry

--- next.config.js
+++ next.config.js
@@ -41,7 +41,7 @@ const config = withSentryConfig(
     // Suppresses source map uploading logs during build
     silent: true,
     org: "mikeh",
-    project: "t3-gallery-video",
+    project: "javascript-nextjs",
   },
   {
     // For all available options, see:
